88c528e881082ea1ff16ead3b8a6d724066c77d4
Jheremi Lema <jheremisj9695@gmail.com>
2026-09-22 19:58:52 +0200

    Estructuración de páginas, main layaout y Rutas

---
+++ src/shared/routes/AppRouter.jsx
@@ -0,0 +1,55 @@
+import { BrowserRouter, Routes, Navigate, Route } from "react-router-dom";
+
+// Importaciones de Layout
+import { MainLayout } from "../../modules/layout";
+
+// Importaciones de Rutas Protegidas
+import { ProtectedRoute } from "../../modules/auth";
+
+// Importaciones de Rutas Públicas
+import { Login } from "../pages/public/Login";
+import { Register } from "../pages/public/Register";
+
+// Importaciones de Rutas Privadas
+import { Dashboard } from "../pages/private/Dashboard";
+import { StudentPages } from "../pages/private/Students";
+import { SessionsPage } from "../pages/private/Sessions";
+  
+
+import { Error404 } from "./Error404";
+
+export const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Definimos las rutas de la App - Plataforma */}
+
+                {/* --- RUTAS PÚBLICAS --- */}
+                <Route path="/login" element={<Login />}/>
+                <Route path="/register" element={<Register />} />
+
+                {/* Redireccionamiento inicial - replace evita que el usuario pueda volver a esta ruta con el boton de retroceder */}
+                <Route path="/" element={<Navigate to={"/dashboard"} replace/>}/>
+
+
+                {/* --- RUTAS PRIVADAS --- */}
+
+                <Route element={<ProtectedRoute />}>
+                    {/* RUTAS DENTRO DE LA CAPA PRINCIPAL QUE COMPARTEN SIDEBAR Y NAVBAR */}
+                    <Route element={<MainLayout />}>
+                        <Route path="/dashboard" element={<Dashboard />}/>
+                        <Route path="/students" element={<Students />}/>
+                        {/* <Route path="/students/:id" element={<StudentDetailPage />} /> */}
+                        <Route path="/sessions" element={<Sessions />} />
+                        {/* <Route path="/" element={<ProfilePage />} /> */}
+
+                    </Route>
+                </Route>
+
+                {/* --- ERROR 404 --- */}
+                <Route path="/404" element={<Error404 />} />
+                <Route path="*" element={<Navigate to={"/404"} replace/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+};  
